ad34e47e772b67fb2fd655504b082043c7b34b34
LimitlessRegret <55678532+LimitlessRegret@users.noreply.github.com>
2019-09-28 13:46:51 +1000

    Add missing WebsocketMessage baseclass

--- EvoS.Framework/Network/NetworkMessages/CheckRAFStatusRequest.cs
+++ EvoS.Framework/Network/NetworkMessages/CheckRAFStatusRequest.cs
@@ -1,10 +1,11 @@
 using System;
+using EvoS.Framework.Network.WebSocket;
 
 namespace EvoS.Framework.Network.NetworkMessages
 {
     [Serializable]
     [EvosMessage(262)]
-    public class CheckRAFStatusRequest
+    public class CheckRAFStatusRequest : WebSocketMessage
     {
         public bool GetReferralCode;
     }
