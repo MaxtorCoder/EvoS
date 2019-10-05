d6d94d1316da85ad5395181c572111a25ec90a55
mts508 <mathyasrodriguez@gmail.com>
2019-10-05 11:58:41 -0300

    DataAccess class for future database

---
+++ EvoS.Framework/DataAccess/DataAccess.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EvoS.Framework.DataAccess
+{
+    class DataAccess
+    {
+        public static string GetMOTD(){ return "New Evos Emulator";}
+        public static string GetReleaseNotesHeader() { return "Exmaple release header"; }
+        public static string GetReleaseNotesDescription() { return "Exmaple release description"; }
+        public static string GetReleaseNotesText() { return "Exmaple release text"; }
+        public static string GetWhatsNewHeader() { return "whats new header"; }
+        public static string GetWhatsNewDescription() { return "whats new header"; }
+        public static string GetWhatsNewText() { return "whats new header"; }
+    }
+}
