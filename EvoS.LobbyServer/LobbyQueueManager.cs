ee5a6cb776d0060876bade331993ce95c64cf9e5
mts508 <mathyasrodriguez@gmail.com>
2019-10-05 12:05:15 -0300

    Make it looks like you joined queue when pressing Ready button

---
+++ EvoS.LobbyServer/LobbyQueueManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EvoS.LobbyServer
+{
+    class LobbyQueueManager
+    {
+        private static LobbyQueueManager Instance = null;
+        private Dictionary<GameType, List<ClientConnection>> Queue;
+
+        private LobbyQueueManager() {
+            Queue = new Dictionary<GameType, List<ClientConnection>>();
+        }
+        
+
+        private static LobbyQueueManager GetInstance() {
+            if (Instance == null) {
+                Instance = new LobbyQueueManager();
+            }
+            return Instance;
+        }
+
+        public static void AddToQueue(GameType queueType, ClientConnection client)
+        {
+            GetInstance();
+            if (!Instance.Queue.ContainsKey(queueType))
+            {
+                Instance.Queue.Add(queueType, new List<ClientConnection>());
+            }
+            Instance.Queue[queueType].Add(client);
+        }
+
+        
+    }
+}
---
+++ EvoS.LobbyServer/NetworkMessageHandlers/JoinMatchmakingQueueRequestHandler.cs
@@ -0,0 +1,48 @@
+﻿using EvoS.Framework.Constants.Enums;
+using EvoS.Framework.Network.NetworkMessages;
+using EvoS.Framework.Network.Static;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvoS.LobbyServer.NetworkMessageHandlers
+{
+    class JoinMatchmakingQueueRequestHandler : IEvosNetworkMessageHandler
+    {
+        public bool DoLogPacket()
+        {
+            return true;
+        }
+
+        public async Task OnMessage(ClientConnection connection, object requestData)
+        {
+            JoinMatchmakingQueueRequest request = (JoinMatchmakingQueueRequest)requestData;
+
+            LobbyQueueManager.AddToQueue(request.GameType, connection);
+
+            JoinMatchmakingQueueResponse response = new JoinMatchmakingQueueResponse();
+            response.ResponseId = request.RequestId;
+            //response.RequestId = request.ResponseId;
+            await connection.SendMessage(response);
+
+            MatchmakingQueueAssignmentNotification assignmentNotification = new MatchmakingQueueAssignmentNotification()
+            {
+                Reason = "",
+                MatchmakingQueueInfo = new LobbyMatchmakingQueueInfo()
+                {
+                    ShowQueueSize = true,
+                    QueuedPlayers = 7,
+                    PlayersPerMinute = 4,
+                    GameConfig = new LobbyGameConfig(),
+                    QueueStatus = QueueStatus.Success,
+                    AverageWaitTime = TimeSpan.FromSeconds(100),
+                    
+                }
+            };
+            await connection.SendMessage(assignmentNotification);
+        }
+
+        
+    }
+}
