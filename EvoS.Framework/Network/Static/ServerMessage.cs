e4882619a3e81ffe5e998d9aca40e39239b7ad6b
mts508 <mathyasrodriguez@gmail.com>
2019-10-05 11:59:38 -0300

    Corrected language names of ServerMessage

--- EvoS.Framework/Network/Static/ServerMessage.cs
+++ EvoS.Framework/Network/Static/ServerMessage.cs
@@ -13,29 +13,29 @@ public class ServerMessage
     {
         return new ServerMessage
         {
-            String_0 = value,
-            String_1 = value,
-            String_2 = value,
-            String_3 = value,
-            String_4 = value,
-            String_5 = value,
-            String_6 = value,
-            String_7 = value,
-            String_8 = value,
-            String_9 = value
+            EN = value,
+            FR = value,
+            DE = value,
+            RU = value,
+            ES = value,
+            IT = value,
+            PL = value,
+            PT = value,
+            KO = value,
+            ZH = value
         };
     }
 
-    public string String_0 { get; set; }
-    public string String_1 { get; set; }
-    public string String_2 { get; set; }
-    public string String_3 { get; set; }
-    public string String_4 { get; set; }
-    public string String_5 { get; set; }
-    public string String_6 { get; set; }
-    public string String_7 { get; set; }
-    public string String_8 { get; set; }
-    public string String_9 { get; set; }
+    public string EN { get; set; }
+    public string FR { get; set; }
+    public string DE { get; set; }
+    public string RU { get; set; }
+    public string ES { get; set; }
+    public string IT { get; set; }
+    public string PL { get; set; }
+    public string PT { get; set; }
+    public string KO { get; set; }
+    public string ZH { get; set; }
 
     /*
 	[JsonIgnore]
