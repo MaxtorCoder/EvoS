a687802981df0fe1b8c0c5629aab9793acd609bd
mts508 <mathyasrodriguez@gmail.com>
2019-10-06 13:17:30 -0300

    ClientStatusReport

---
+++ EvoS.Framework/Network/NetworkMessages/ClientStatusReport.cs
@@ -0,0 +1,29 @@
+﻿using EvoS.Framework.Network.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EvoS.Framework.Network.NetworkMessages
+{
+    [Serializable]
+    [EvosMessage(696, typeof(ClientStatusReport))]
+    class ClientStatusReport : WebSocketMessage
+    {
+        public string DeviceIdentifier;
+        public string FileDateTime;
+        public ClientStatusReport.ClientStatusReportType Status;
+        public string StatusDetails;
+        public string UserMessage;
+
+        [Serializable]
+        [EvosMessage(697, typeof(ClientStatusReport.ClientStatusReportType))]
+        public enum ClientStatusReportType
+        {
+			u001D,
+			u000E,
+			u0012,
+			u0015,
+			u0016
+		}
+    }
+}
