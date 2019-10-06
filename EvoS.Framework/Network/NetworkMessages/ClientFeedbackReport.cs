55eb7bbcbe9dec589635c2961d48e6705e9e1b87
mts508 <mathyasrodriguez@gmail.com>
2019-10-06 11:08:01 -0300

    Basic Feedback handling

---
+++ EvoS.Framework/Network/NetworkMessages/ClientFeedbackReport.cs
@@ -0,0 +1,35 @@
+﻿using EvoS.Framework.Network.Static;
+using EvoS.Framework.Network.WebSocket;
+using System;
+
+
+namespace EvoS.Framework.Network.NetworkMessages
+{
+    [Serializable]
+    [EvosMessage(687, typeof(ClientFeedbackReport))]
+    public class ClientFeedbackReport : WebSocketMessage
+    {
+        public string Message;
+        public ClientFeedbackReport.FeedbackReason Reason;
+        public long ReportedPlayerAccountId;
+        public string ReportedPlayerHandle;
+
+        [Serializable]
+        [EvosMessage(688, typeof(ClientFeedbackReport.FeedbackReason))]
+        public enum FeedbackReason
+        {
+			Suggestion,
+			Bug,
+			u0012,
+			u0015,
+			u0016,
+			u0013,
+			u0018,
+			u0009,
+			u0019,
+			u0011,
+			u001A,
+			u0004
+		}
+    }
+}
---
+++ EvoS.LobbyServer/NetworkMessageHandlers/ClientFeedbackReportHandler.cs
@@ -0,0 +1,27 @@
+﻿using EvoS.Framework.Network.NetworkMessages;
+using EvoS.Framework.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvoS.LobbyServer.NetworkMessageHandlers
+{
+    class ClientFeedbackReportHandler : IEvosNetworkMessageHandler
+    {
+        public bool DoLogPacket()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task OnMessage(ClientConnection connection, object requestData)
+        {
+            ClientFeedbackReport feedback = (ClientFeedbackReport)requestData;
+            if (feedback.Reason == ClientFeedbackReport.FeedbackReason.Suggestion)
+                Log.Print(LogType.Lobby, $"{connection.RegistrationInfo.SessionInfo.UserName} sent a suggestion: {feedback.Message}");
+            else if (feedback.Reason == ClientFeedbackReport.FeedbackReason.Bug)
+                Log.Print(LogType.Lobby, $"{connection.RegistrationInfo.SessionInfo.UserName} sent a bug report: {feedback.Message}");
+            return Task.CompletedTask;
+        }
+    }
+}
