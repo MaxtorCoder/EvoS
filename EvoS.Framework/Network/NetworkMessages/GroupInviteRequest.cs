70eeb2cca19c69bcb93e5509754e4cecfd8d386f
mts508 <mathyasrodriguez@gmail.com>
2019-10-09 23:37:08 -0300

    Group invitation request

---
+++ EvoS.Framework/Network/NetworkMessages/GroupInviteRequest.cs
@@ -0,0 +1,14 @@
+﻿using EvoS.Framework.Network.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EvoS.Framework.Network.NetworkMessages
+{
+    [Serializable]
+    [EvosMessage(381)]
+    public class GroupInviteRequest : WebSocketMessage
+    {
+        public string FriendHandle;
+    }
+}
---
+++ EvoS.Framework/Network/NetworkMessages/GroupInviteResponse.cs
@@ -0,0 +1,15 @@
+﻿using EvoS.Framework.Network.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EvoS.Framework.Network.NetworkMessages
+{
+    [Serializable]
+    [EvosMessage(380)]
+    class GroupInviteResponse : WebSocketResponseMessage
+    {
+        public string FriendHandle;
+        public LocalizationPayload LocalizedFailure;
+    }
+}
