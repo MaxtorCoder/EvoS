2c9147c0ee970689b04c6da1431ef3b24a4c0c3a
mts508 <mathyasrodriguez@gmail.com>
2019-10-09 22:58:41 -0300

    StoreOpenedMessage

---
+++ EvoS.Framework/Network/NetworkMessages/StoreOpenedMessage.cs
@@ -0,0 +1,18 @@
+﻿using EvoS.Framework.Network.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EvoS.Framework.Network.NetworkMessages
+{
+    /*
+    * Sent by the player when it opens the Collections tab from the main menu
+    * note: Have no idea what response/notification send to this
+    */
+    [Serializable]
+    [EvosMessage(350)]
+    public class StoreOpened : WebSocketMessage
+    {
+        
+    }
+}
