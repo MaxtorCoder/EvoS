536f024bf88d5b18d9c8e3ad23b28f24ba6dc8f4
mts508 <mathyasrodriguez@gmail.com>
2019-10-09 22:55:42 -0300

    Can open account info when clicking on banner

---
+++ EvoS.LobbyServer/NetworkMessageHandlers/PlayerMatchDataRequestHandler.cs
@@ -0,0 +1,22 @@
+﻿using EvoS.Framework.Network.NetworkMessages;
+using EvoS.Framework.Network.Static;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvoS.LobbyServer.NetworkMessageHandlers
+{
+    class PlayerMatchDataRequestHandler : IEvosNetworkMessageHandler
+    {
+        public async Task OnMessage(ClientConnection connection, object requestData)
+        {
+            var response = new PlayerMatchDataResponse()
+            {
+                MatchData = new List<PersistedCharacterMatchData>(),
+                ResponseId = ((PlayerMatchDataRequest)requestData).RequestId
+            };
+            await connection.SendMessage(response);
+        }
+    }
+}
