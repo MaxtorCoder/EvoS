431552baa25b95e2fce623001b6123de43a901c6
mts508 <mathyasrodriguez@gmail.com>
2019-10-12 01:50:25 -0300

    Set GameMode handler

---
+++ EvoS.LobbyServer/NetworkMessageHandlers/SetGameSubTypeRequestHandler.cs
@@ -0,0 +1,18 @@
+﻿using EvoS.Framework.Network.NetworkMessages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvoS.LobbyServer.NetworkMessageHandlers
+{
+    class SetGameSubTypeRequestHandler : IEvosNetworkMessageHandler
+    {
+        public async Task OnMessage(ClientConnection connection, object requestData)
+        {
+            SetGameSubTypeRequest request = (SetGameSubTypeRequest)requestData;
+            connection.SelectedGameType = request.gameType;
+            await connection.SendMessage(new SetGameSubTypeResponse() { ResponseId = request.RequestId });
+        }
+    }
+}
