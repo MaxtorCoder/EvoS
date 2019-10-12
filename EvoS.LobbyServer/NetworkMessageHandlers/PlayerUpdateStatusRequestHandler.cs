93b4047436086663917341ff15d13bc61cd2c084
mts508 <mathyasrodriguez@gmail.com>
2019-10-12 01:51:21 -0300

    Save Player Status

---
+++ EvoS.LobbyServer/NetworkMessageHandlers/PlayerUpdateStatusRequestHandler.cs
@@ -0,0 +1,24 @@
+﻿using EvoS.Framework.Network.NetworkMessages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvoS.LobbyServer.NetworkMessageHandlers
+{
+    class PlayerUpdateStatusRequestHandler : IEvosNetworkMessageHandler
+    {
+        public async Task OnMessage(ClientConnection connection, object requestData)
+        {
+            PlayerUpdateStatusRequest request = (PlayerUpdateStatusRequest)requestData;
+            connection.StatusString = request.StatusString;
+            var response = new PlayerUpdateStatusResponse()
+            {
+                AccountId = request.AccountId,
+                StatusString = request.StatusString,
+                ResponseId = request.RequestId
+            };
+            await connection.SendMessage(response);
+        }
+    }
+}
