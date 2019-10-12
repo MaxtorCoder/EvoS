de334c51235f1e269b87052de683d65d6fe78d83
mts508 <mathyasrodriguez@gmail.com>
2019-10-12 01:49:08 -0300

    PlayerLoadout for ClientConnection

---
+++ EvoS.Framework/Misc/PlayerLoadout.cs
@@ -0,0 +1,23 @@
+﻿using EvoS.Framework.Network.Static;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EvoS.Framework.Misc
+{
+    public class PlayerLoadout
+    {
+        public CharacterVisualInfo Skin;
+        public CharacterModInfo Mods;
+        public CharacterCardInfo Cards;
+        public CharacterAbilityVfxSwapInfo AbilityVfxSwaps;
+
+        public PlayerLoadout()
+        {
+            Skin = new CharacterVisualInfo(0, 0, 0);
+            Mods.Reset();
+            Cards.Reset();
+            AbilityVfxSwaps.Reset();
+        }
+    }
+}
