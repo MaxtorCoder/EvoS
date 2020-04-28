cc158d91c84acba803d8fb6dc1e3846f7e589a90
mts508 <mathyasrodriguez@gmail.com>
2020-04-27 22:04:44 -0300

    Added missing rejoin game request and response

---
+++ EvoS.Framework/Network/NetworkMessages/RejoinGameRequest.cs
@@ -0,0 +1,16 @@
+﻿using EvoS.Framework.Network.Static;
+using EvoS.Framework.Network.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EvoS.Framework.Network.NetworkMessages
+{
+    [Serializable]
+    [EvosMessage(475)]
+    public class RejoinGameRequest : WebSocketMessage
+    {
+        public LobbyGameInfo PreviousGameInfo;
+        public bool Accept;
+    }
+}
---
+++ EvoS.Framework/Network/NetworkMessages/RejoinGameResponse.cs
@@ -0,0 +1,14 @@
+﻿using EvoS.Framework.Network.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EvoS.Framework.Network.NetworkMessages
+{
+    [Serializable]
+    [EvosMessage(474)]
+    public class RejoinGameResponse : WebSocketResponseMessage
+    {
+
+    }
+}
